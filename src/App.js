1e1a04611e7356d98a5ec5207a0f70dea5863ac5
JessieZevaka <101160859+JessieZevaka@users.noreply.github.com>
2022-07-05 16:40:02 +1000

    Created search component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
